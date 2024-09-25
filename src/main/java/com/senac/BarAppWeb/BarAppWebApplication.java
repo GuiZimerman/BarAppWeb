d2dc63aad5df1937a8d7451eb54bce85684e44ee
Guilherme Zimerman <guilhermezimerman3@gmail.com>
2024-09-25 15:59:47 -0300

    Criação do Projeto BarAppWeb

---
+++ src/main/java/com/senac/BarAppWeb/BarAppWebApplication.java
@@ -0,0 +1,13 @@
+package com.senac.BarAppWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarAppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarAppWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/BarAppWeb/BarAppWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.BarAppWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarAppWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
