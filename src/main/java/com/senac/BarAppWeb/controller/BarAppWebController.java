0343d38cab9e61f1cf7fa2a7f0f7c276b1271bd1
Guilherme Zimerman <guilhermezimerman3@gmail.com>
2024-09-30 14:53:36 -0300

    Atualização da página CadastroCliente

--- src/main/java/com/senac/BarAppWeb/controller/BarAppWebController.java
+++ src/main/java/com/senac/BarAppWeb/controller/BarAppWebController.java
@@ -1,19 +1,25 @@
 package com.senac.BarAppWeb.controller;
 
 
+import com.senac.BarAppWeb.model.Cliente;
 import com.senac.BarAppWeb.model.Conta;
+import com.senac.BarAppWeb.service.ClienteService;
 import com.senac.BarAppWeb.service.ContaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BarAppWebController {
     
     @Autowired
     ContaService contaService;
+    @Autowired
+    ClienteService clienteService;
     
 //    @GetMapping("/")
 //    public String mostraInicial() {
@@ -25,6 +31,18 @@ public class BarAppWebController {
         List<Conta> listaContasAbertas = contaService.findAll();
         model.addAttribute("listaContaAbertas", listaContasAbertas);
         return "atendimento";
+    }
+    
+    @GetMapping("/cadastroCliente")
+    public String mostrarCadastroCliente(Model model) {
+        return "cadastroCliente";
+    }
+    
+    @PostMapping("/cadastroCliente")
+    public String fazerCadastro(@ModelAttribute("cliente") Cliente novoCliente, Model model) {
+       clienteService.criarCliente(novoCliente);
+       model.addAttribute("mensagem", "Cliente cadastrado com sucesso!");
+       return "atendimento";
     }
     
 }
