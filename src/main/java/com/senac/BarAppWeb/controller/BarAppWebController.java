e888ffa0b28dbcdfe2c089239fa96bd8b0a47108
Guilherme Zimerman <guilhermezimerman3@gmail.com>
2024-09-30 10:48:09 -0300

    Implementações na página Atendimento

--- src/main/java/com/senac/BarAppWeb/controller/BarAppWebController.java
+++ src/main/java/com/senac/BarAppWeb/controller/BarAppWebController.java
@@ -1,5 +1,21 @@
 package com.senac.BarAppWeb.controller;
 
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class BarAppWebController {
+    
+    @GetMapping("/")
+    public String mostraInicial() {
+        return "redirect:/index";
+    }
+    
+    @GetMapping("/atendimento")
+    public String mostraAtendimento(Model model){
+        return "atendimento";
+    }
+    
 }
+
