6584858e0dfd2c44cba3e72e7989af5849830a33
Guilherme Zimerman <guilhermezimerman3@gmail.com>
2024-10-01 02:35:32 -0300

    Validações para cadastro de nome e alteração de fundo

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -34,3 +34,48 @@ $(document).ready(function () {
         alert("ID selecionado: " + selectedId); 
     });
 });
+
+//Validações CadastroCliente
+ $(document).ready(function() {
+                $('#clienteForm').on('submit', function(event) {
+                    event.preventDefault();
+                    
+                    if (emptyFields() || !validaNome() || !validaTelefone()) {
+                        return; // 
+                    }
+                    
+                    this.submit();
+                });
+                
+                function emptyFields() {
+                    let nome = $('#nome').val().trim();
+                    let telefone = $('#telefone').val().trim();
+
+                    if (nome === '') {
+                        alert("Nome do cliente não pode estar vazio.");
+                        return true;
+                    } else if (telefone === '') {
+                        alert("Telefone não pode estar vazio.");
+                        return true;
+                    }
+                    return false;
+                }
+
+                function validaNome() {
+                    let nome = $('#nome').val().trim();
+                    if (!/^[\p{L}'\s]+$/u.test(nome)) {
+                        alert("Nome do cliente inválido.");
+                        return false;
+                    }
+                    return true;
+                }
+
+                function validaTelefone() {
+                    let telefone = $('#telefone').val().replace(/\D/g, ''); 
+                    if (!/^\d{10,11}$/.test(telefone)) {
+                        alert("Número de telefone inválido.\nInsira o telefone. Ex: (DD)981175456");
+                        return false;
+                    }
+                    return true;
+                }
+            });
