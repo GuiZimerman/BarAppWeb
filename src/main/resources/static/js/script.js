f2921faf12478e9dab284cfe1a82af0b2d268dcb
Guilherme Zimerman <guilhermezimerman3@gmail.com>
2024-09-30 21:47:31 -0300

    Configuração da função de selecionar nas tabelas

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,7 +1,36 @@
-$(document).ready(function() {
-        $('#contaTabela tbody tr').click(function() {
-            $('#contaTabela tbody tr').removeClass('selected');
-            $(this).addClass('selected');
-        });
+var selectedRow = null; 
+var selectedId = null; 
+
+// Função para a tabela com id 'contasAbertasTbl'
+$(document).ready(function () {
+    $('#contasAbertasTbl tbody tr').click(function () {
+        if (selectedRow) {
+            $(selectedRow).removeClass('selected');
+        }
+
+        $(this).addClass('selected'); 
+        selectedRow = this; 
+
+        selectedId = $(this).find('td')[0].innerText;
+        alert("ID selecionado: " + selectedId); 
     });
-    
+});
+
+// Função para a tabela com id 'abrirContaTbl'
+$(document).ready(function () {
+    var selectedRow = null;  
+    var selectedId = null;   
+
+    $('#abrirContaTbl tbody tr').click(function () {
+        if (selectedRow) {
+            $(selectedRow).removeClass('selected'); 
+        }
+
+        $(this).addClass('selected');  
+        selectedRow = this;           
+
+        
+        selectedId = $(this).children('td:first').text();
+        alert("ID selecionado: " + selectedId); 
+    });
+});
